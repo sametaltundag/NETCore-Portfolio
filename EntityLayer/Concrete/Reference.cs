fc4c0d056add37db77353b3874e37420557815c1
Samet ALTUNDAG <smtaltundag63@gmail.com>
2024-02-06 01:07:22 +0300

    Entity Fw Core Paketleri katmanlara yüklendi.

---
+++ EntityLayer/Concrete/Reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Reference
+    {
+        [Key]
+        public int ReferenceID { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
